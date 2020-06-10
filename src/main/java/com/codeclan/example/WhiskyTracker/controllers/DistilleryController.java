b06688779396f3b0f1e52a535418dab74cc3984d
Gordon Soutter <gordon.soutter@outlook.com>
2020-06-10 20:42:36 +0100

    adds get all distileries route to distillery controller

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,9 +1,15 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 //@RequestMapping(value = "/distilleries")
@@ -12,6 +18,11 @@ public class DistilleryController {
     @Autowired
     DistilleryRepository distilleryRepository;
 
+    // get on /distilleries
+    @GetMapping(value = "distilleries")
+    public ResponseEntity<List<Distillery>> getAllDistillers(){
+        return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+    }
 
 
 }
